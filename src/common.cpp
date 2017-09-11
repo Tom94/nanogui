6c14a06e1b57333c96ef289acd03880572ce3081
Thomas Müller <thomas94@gmx.net>
2017-09-11 15:14:49 +0200

    Avoid code duplication

--- src/common.cpp
+++ src/common.cpp
@@ -202,9 +202,18 @@ loadImageDirectory(NVGcontext *ctx, const std::string &path) {
     return result;
 }
 
-#if !defined(__APPLE__)
 std::string file_dialog(const std::vector<std::pair<std::string, std::string>> &filetypes, bool save) {
-    static const int FILE_DIALOG_MAX_BUFFER = 1024;
+    auto result = file_dialog(filetypes, save, false);
+    return result.empty() ? "" : result.front();
+}
+
+#if !defined(__APPLE__)
+std::vector<std::string> file_dialog(const std::vector<std::pair<std::string, std::string>> &filetypes, bool save, bool multiple) {
+    static const int FILE_DIALOG_MAX_BUFFER = 16384;
+    if (save && multiple) {
+        throw std::invalid_argument("save and multiple must not both be true.");
+    }
+
 #if defined(_WIN32)
     OPENFILENAME ofn;
     ZeroMemory(&ofn, sizeof(OPENFILENAME));
@@ -235,7 +244,7 @@ std::string file_dialog(const std::vector<std::pair<std::string, std::string>> &
         }
         filter.push_back('\0');
     }
-    for (auto pair: filetypes) {
+    for (auto pair : filetypes) {
         filter.append(pair.second);
         filter.append(" (*.");
         filter.append(pair.first);
@@ -251,84 +260,14 @@ std::string file_dialog(const std::vector<std::pair<std::string, std::string>> &
     if (save) {
         ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT;
         if (GetSaveFileNameA(&ofn) == FALSE)
-            return "";
+            return {};
     } else {
         ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+        if (multiple)
+            ofn.Flags |= OFN_ALLOWMULTISELECT;
         if (GetOpenFileNameA(&ofn) == FALSE)
-            return "";
+            return {};
     }
-    return std::string(ofn.lpstrFile);
-#else
-    char buffer[FILE_DIALOG_MAX_BUFFER];
-    buffer[0] = '\0';
-
-    std::string cmd = "/usr/bin/zenity --file-selection ";
-    if (save)
-        cmd += "--save ";
-    cmd += "--file-filter=\"";
-    for (auto pair: filetypes)
-        cmd += "\"*." + pair.first +  "\" ";
-    cmd += "\"";
-    FILE *output = popen(cmd.c_str(), "r");
-    if (output == nullptr)
-        throw std::runtime_error("popen() failed -- could not launch zenity!");
-    while (fgets(buffer, FILE_DIALOG_MAX_BUFFER, output) != NULL)
-        ;
-    pclose(output);
-    std::string result(buffer);
-    result.erase(std::remove(result.begin(), result.end(), '\n'), result.end());
-    return result;
-#endif
-}
-
-std::vector<std::string> file_dialog_multiple(const std::vector<std::pair<std::string, std::string>> &filetypes) {
-    static const int FILE_DIALOG_MAX_BUFFER = 16384;
-#if defined(_WIN32)
-    OPENFILENAME ofn;
-    ZeroMemory(&ofn, sizeof(OPENFILENAME));
-    ofn.lStructSize = sizeof(OPENFILENAME);
-    char tmp[FILE_DIALOG_MAX_BUFFER];
-    ofn.lpstrFile = tmp;
-    ZeroMemory(tmp, FILE_DIALOG_MAX_BUFFER);
-    ofn.nMaxFile = FILE_DIALOG_MAX_BUFFER;
-    ofn.nFilterIndex = 1;
-
-    std::string filter;
-
-    if (filetypes.size() > 1) {
-        filter.append("Supported file types (");
-        for (size_t i = 0; i < filetypes.size(); ++i) {
-            filter.append("*.");
-            filter.append(filetypes[i].first);
-            if (i + 1 < filetypes.size())
-                filter.append(";");
-        }
-        filter.append(")");
-        filter.push_back('\0');
-        for (size_t i = 0; i < filetypes.size(); ++i) {
-            filter.append("*.");
-            filter.append(filetypes[i].first);
-            if (i + 1 < filetypes.size())
-                filter.append(";");
-        }
-        filter.push_back('\0');
-    }
-    for (auto pair : filetypes) {
-        filter.append(pair.second);
-        filter.append(" (*.");
-        filter.append(pair.first);
-        filter.append(")");
-        filter.push_back('\0');
-        filter.append("*.");
-        filter.append(pair.first);
-        filter.push_back('\0');
-    }
-    filter.push_back('\0');
-    ofn.lpstrFilter = filter.data();
-
-    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_ALLOWMULTISELECT;
-    if (GetOpenFileNameA(&ofn) == FALSE)
-        return {};
 
     size_t i = 0;
     std::vector<std::string> result;
@@ -349,10 +288,14 @@ std::vector<std::string> file_dialog_multiple(const std::vector<std::pair<std::s
     char buffer[FILE_DIALOG_MAX_BUFFER];
     buffer[0] = '\0';
 
+    std::string cmd = "/usr/bin/zenity --file-selection ";
     // The safest separator for multiple selected paths is /, since / can never occur
     // in file names. Only where two paths are concatenated will there be two / following
     // each other.
-    std::string cmd = "/usr/bin/zenity --file-selection --multiple --separator=\"/\" ";
+    if (multiple)
+        cmd += "--multiple --separator=\"/\" ";
+    if (save)
+        cmd += "--save ";
     cmd += "--file-filter=\"";
     for (auto pair : filetypes)
         cmd += "\"*." + pair.first + "\" ";
