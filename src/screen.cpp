a700b49f0af83e04c1f7c06397947bd5d0e48eb0
Thomas Müller <thomas94@gmx.net>
2017-10-10 17:50:53 +0200

    Remove hardcoded cursor position offset

--- src/screen.cpp
+++ src/screen.cpp
@@ -491,8 +491,6 @@ bool Screen::cursorPosCallbackEvent(double x, double y) {
     bool ret = false;
     mLastInteraction = glfwGetTime();
     try {
-        p -= Vector2i(1, 2);
-
         if (!mDragActive) {
             Widget *widget = findWidget(p);
             if (widget != nullptr && widget->cursor() != mCursor) {
